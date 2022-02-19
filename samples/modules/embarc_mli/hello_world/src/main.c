a685ea021248d808a7e958e73e4fb0bc5f9e3f4b
Siyuan Cheng <siyuanc@synopsys.com>
2022-02-19 17:35:09 +0800

    TEST: test hello_world
    test hello_world with latest Zephyr and MLI
    
    Signed-off-by: Siyuan Cheng <siyuanc@synopsys.com>

--- samples/modules/embarc_mli/hello_world/src/main.c
+++ samples/modules/embarc_mli/hello_world/src/main.c
@@ -8,47 +8,76 @@
 #include <assert.h>
 #include <zephyr.h>
 #include <stdio.h>
+#include "mli_config.h"
 
-int main(void)
-{
-	printf("************************************\n");
-	int8_t data_in[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-	mli_tensor in = { 0 };
+#if (PLATFORM == V2DSP_XY)
+#define DATA_ATTR __xy __attribute__((section(".Xdata")))
 
-	in.el_type = MLI_EL_FX_8;
-	in.rank = 1;
-	in.shape[0] = 8;
-	in.mem_stride[0] = 1;
-	in.capacity = sizeof(data_in);
-	in.data = data_in;
+#elif (PLATFORM == V2DSP_VECTOR)
+#define DATA_ATTR __vccm __attribute__((section(".vecmem_data")))
 
-	int8_t data_out[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-	mli_tensor out = { 0 };
+#else
+#define DATA_ATTR
 
-	out.el_type = MLI_EL_FX_8;
-	out.rank = 1;
-	out.shape[0] = 8;
-	out.mem_stride[0] = 1;
-	out.capacity = sizeof(data_out);
-	out.data = data_out;
+#endif
 
-	mli_status status;
+#define NUM_ELEMS 8
 
-	status = mli_krn_eltwise_add_fx8(&in, &in, &out);
-	assert(status == MLI_STATUS_OK);
-	for (int i = 0; i < ARRAY_SIZE(data_out); i++) {
-		printf("%d ", ((int8_t *)(out.data))[i]);
-	}
-	printf("\n");
+int16_t DATA_ATTR data_in1[NUM_ELEMS] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+int16_t DATA_ATTR data_in2[NUM_ELEMS] = { 10, 20, 30, 40, 50, 60, 70, 80 };
+int16_t DATA_ATTR data_out[NUM_ELEMS] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-	status = mli_krn_eltwise_sub_fx8(&in, &in, &out);
-	assert(status == MLI_STATUS_OK);
-	for (int i = 0; i < ARRAY_SIZE(data_out); i++) {
-		printf("%d ", ((int8_t *)(out.data))[i]);
-	}
-	printf("\n");
-	printf("************************************\n");
+int main() {
+    mli_tensor in1 = { 0 };
+    in1.el_type = MLI_EL_FX_16;
+    in1.rank = 1;
+    in1.shape[0] = NUM_ELEMS;
+    in1.mem_stride[0] = 1;
+    in1.data.capacity = sizeof(data_in1);
+    in1.data.mem.pi16 = (int16_t *)data_in1;
 
-	printf("Hello World! %s\n", CONFIG_BOARD);
-	return 0;
+    mli_tensor in2 = { 0 };
+    in2.el_type = MLI_EL_FX_16;
+    in2.rank = 1;
+    in2.shape[0] = NUM_ELEMS;
+    in2.mem_stride[0] = 1;
+    in2.data.capacity = sizeof(data_in2);
+    in2.data.mem.pi16 = (int16_t *)data_in2;
+
+    mli_tensor out = { 0 };
+    out.el_type = MLI_EL_FX_16;
+    out.rank = 1;
+    out.shape[0] = NUM_ELEMS;
+    out.mem_stride[0] = 1;
+    out.data.capacity = sizeof(data_out);
+    out.data.mem.pi16 = (int16_t *)data_out;
+
+    printf("in1:\n");
+    for (int i = 0; i < NUM_ELEMS; i++) {
+        printf("%d ", in1.data.mem.pi16[i]);
+    }
+    printf("\nin2:\n");
+    for (int i = 0; i < NUM_ELEMS; i++) {
+        printf("%d ", in2.data.mem.pi16[i]);
+    }
+
+    mli_status status;
+    status = mli_krn_eltwise_add_fx16(&in1, &in2, &out);
+    assert(status == MLI_STATUS_OK);
+
+    printf("\nmli_krn_eltwise_add_fx16 output:\n");
+    for (int i = 0; i < NUM_ELEMS; i++) {
+        printf("%d ", out.data.mem.pi16[i]);
+    }
+
+    status = mli_krn_eltwise_sub_fx16(&in1, &in2, &out);
+    assert(status == MLI_STATUS_OK);
+
+    printf("\nmli_krn_eltwise_sub_fx16 output:\n");
+    for (int i = 0; i < NUM_ELEMS; i++) {
+        printf("%d ", out.data.mem.pi16[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
